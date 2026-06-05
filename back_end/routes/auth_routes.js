becf127dcfc7396f34903689a2a098f5426d67ab
Nikhil-VIP <nikhilchauha007@gmail.com>
2026-06-05 20:53:20 +0530

    ok

--- back_end/routes/auth_routes.js
+++ back_end/routes/auth_routes.js
@@ -1,1 +1,6 @@
 const express = require('express');
+const router = express.Router();
+const { new_user, login_user } = require('../controllers/authcontroller.js');
+router.post('/register', new_user);
+router.post('/login', login_user);
+module.exports = router;
