89d56cf5891728699dbff09a2c6c77cdf062f951
Nikhil-VIP <nikhilchauha007@gmail.com>
2026-06-07 20:25:20 +0530

    server js is almost done

--- back_end/server.js
+++ back_end/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const express = require('express');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
+app.use(express.json());
 app.use(cors());
+mongoose.connect(process.env.mongo_uri)
+  .then(() => console.log("connected to db"))
+  .catch((err) => console.error("Error connecting to db:", err));
 app.get('/', (req, res) => {
   res.send("hiiii")
 });
