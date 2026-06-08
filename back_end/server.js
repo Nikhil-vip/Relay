d10f1cc099e6b0a1848cd7a2ff992864466acf18
Nikhil-VIP <nikhilchauha007@gmail.com>
2026-06-08 20:32:55 +0530

    connected all backend files together i mean "Stiched" togher i am a pro level coderrrr

--- back_end/server.js
+++ back_end/server.js
@@ -3,16 +3,29 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config();
+const authRotes = require('..routes/auth_routes.js')
 const app = express();
+
+dotenv.config();
+
+app.use('/api/auth', authRotes);
 app.use(express.json());
 app.use(cors());
+
 mongoose.connect(process.env.mongo_uri)
+
   .then(() => console.log("connected to db"))
   .catch((err) => console.error("Error connecting to db:", err));
+
+
 app.get('/', (req, res) => {
+
   res.send("hiiii")
+
 });
+
 app.listen(3000, () => {
+
   console.log("server running on port 3000");
+
 })
